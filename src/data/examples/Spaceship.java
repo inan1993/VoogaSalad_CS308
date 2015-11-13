7a965d919ad9688677cd17e0efc8c8ec52056e93
Austin Liu <abl17@duke.edu>
2015-11-12 21:04:14 -0500

    Fixed SpaceShip errors

--- src/data/examples/Spaceship.java
+++ src/data/examples/Spaceship.java
@@ -2,7 +2,9 @@ package data.examples;
 
 import authoring.model.actions.IAction;
 import authoring.model.actors.IActor;
+import authoring.model.bundles.Bundle;
 import authoring.model.properties.IProperties;
+import authoring.model.properties.Property;
 import authoring.model.triggers.selftriggers.ASelfTrigger;
 import javafx.scene.paint.Color;
 
@@ -24,25 +26,30 @@ public class Spaceship implements IActor {
 		mySize = size;
 		myColor = color;
 	}
+	@Override
+	public Bundle<Property<?>> getProperties() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
-	@Override
-	public IProperties getProperties() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	@Override
-	public ASelfTrigger getSelfTriggers() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	@Override
-	public IAction getActions() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	@Override
-	public void addSelfTrigger(ASelfTrigger e1) {
-		// TODO Auto-generated method stub
-		
-	}
+//	@Override
+//	public IProperties getProperties() {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//	@Override
+//	public ASelfTrigger getSelfTriggers() {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//	@Override
+//	public IAction getActions() {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//	@Override
+//	public void addSelfTrigger(ASelfTrigger e1) {
+//		// TODO Auto-generated method stub
+//		
+//	}
 }
