ffbbff70a6f70e6294021b03664cc6173245d574
duckgirl07 <bridget.dou@gmail.com>
2015-11-14 22:27:17 -0500

    oh hey crucial changes present.

--- src/view/screen/CreatorScreen.java
+++ src/view/screen/CreatorScreen.java
@@ -3,6 +3,7 @@ package view.screen;
 import java.util.ArrayList;
 
 import javafx.scene.Scene;
+import javafx.scene.control.CheckMenuItem;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import view.element.ActorBrowser;
@@ -16,15 +17,25 @@ public class CreatorScreen extends AbstractScreen {
 	private ArrayList<GridPane> dockPanes;
 	private ArrayList<GridPane> homePanes;
 	private ActorBrowser browser;
+	private CheckMenuItem checkFullscreen;
 
 	public CreatorScreen() {
 		findResources();
 		WIDTH = Integer.parseInt(myResources.getString("width"));
 		HEIGHT = Integer.parseInt(myResources.getString("height"));
+		checkFullscreen = new CheckMenuItem("Full Screen");
+		checkFullscreen.setSelected(true);
+		checkFullscreen.selectedProperty().addListener(e -> maxOrMin(checkFullscreen.isSelected()));
+		
 		makeScene();
 		root.prefHeightProperty().bind(scene.heightProperty());
 		root.prefWidthProperty().bind(scene.widthProperty());
 		this.title = myResources.getString("title");
+	}
+
+	//TODO: this should work once the observer/observable relationship between stage and scene is fully set up.
+	private void maxOrMin(boolean selected) {
+		fullscreen = selected;
 	}
 
 	@Override
@@ -56,5 +67,9 @@ public class CreatorScreen extends AbstractScreen {
 		bottomPane.add(homePanes.get(2), 1, 0);
 		r.setBottom(bottomPane);
 		browser = new ActorBrowser(dockPanes.get(0), homePanes.get(0), myResources.getString("browsername"), this);
+		
+		// this line seems like poor design but let's go with it for now
+		t.addToToolbar(checkFullscreen);
+		t.addToToolbar(browser.getMenuItem());
 	}
 }
